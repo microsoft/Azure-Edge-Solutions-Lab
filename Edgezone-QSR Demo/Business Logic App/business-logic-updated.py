625f6e1bb4f1ef05e97b90e7c54f54034411e290
Swapnil Sunilkumar Bhasale <sbhasale@microsoft.com>
2022-12-06 13:29:00 -0800

    edgezone qsr demo first commit

---
+++ Edgezone-QSR Demo/Business Logic App/business-logic-updated.py
@@ -0,0 +1,87 @@
+import json
+import logging
+import time
+from datetime import datetime
+
+import paho.mqtt.client as mqtt
+
+broker_url = "13.105.113.2"
+broker_port = 1883
+orders = []
+
+#define callback
+def on_message(client, userdata, message):
+    
+    #decode the incoming message 
+    # sample json data
+        # {'bag_no': 1, 'item_name': 'soda', 'action': 'add'}
+    json_in_order = json.loads(str(message.payload.decode("utf-8","ignore"))) #decode json data
+    print("Action: ", json_in_order)
+    # fetch order to check 
+    # {"bag_no" : 1, "items" : [{"French Fries" : 1, "Cheeseburger" : 1, "Soda" : 1, "Chicken Nuggets" : 1}]}
+    order = [""]
+    for i in range(0, len(orders)):
+        if orders[i]["bag_no"] == json_in_order["bag_no"]:
+            order = orders[i]["items"]
+    # order is a key-value hashmap
+    if json_in_order["item_name"] in order[0].keys():
+        if json_in_order["action"] == "add":
+            json_out = json_in_order
+            json_out["Status"] = True
+            order[0][json_in_order["item_name"]] -= 1  
+            if isOrderComplete(order[0]):
+                order_complete = {"bag_no": json_in_order["bag_no"], "Status": "order-completed"}
+                print("Messages to Publish: ", order_complete)
+                client.publish(topic="result", payload=json.dumps(order_complete), qos=1, retain=False)          
+        elif json_in_order["action"] == "remove":
+            json_out = json_in_order
+            json_out["Status"] = False
+    elif json_in_order["item_name"] not in order[0].keys():
+        if json_in_order["action"] == "add":
+            json_out = json_in_order
+            json_out["Status"] = False
+        elif json_in_order["action"] == "remove":
+            json_out = json_in_order
+            json_out["Status"] = True
+            if isOrderComplete(order[0]):
+                order_complete = {"bag_no": json_in_order["bag_no"], "Status": "order-completed"}
+                print("Messages to Publish: ", order_complete)
+                client.publish(topic="result", payload=json.dumps(order_complete), qos=1, retain=False)
+            
+    print("Messages to Publish: ", json_out)    
+    #publish the result to broker
+    client.publish(topic="result", payload=json.dumps(json_out), qos=1, retain=False)
+
+def on_message_neworder(client, userdata, message):
+    #decode the incoming message 
+    order = json.loads(str(message.payload.decode("utf-8","ignore"))) #decode json data
+    # {"bag_no" : 1, "items" : [{"French Fries" : 1, "Cheeseburger" : 1, "Soda" : 1, "Chicken Nuggets" : 1}]}
+    print("Order received from PoS application", order)
+    orders.append(order)
+
+def isOrderComplete(order):
+    for value in order.values():
+       if value >= 1:
+        return False
+    return True
+
+#configure the client and connect 
+client = mqtt.Client()
+#client.on_message = on_message
+client.message_callback_add("orders", on_message)
+client.message_callback_add("new-order", on_message_neworder)
+client.username_pw_set(username="client2", password="password2")
+client.connect(broker_url, broker_port, keepalive=800)
+
+# start loop to process recieved messages
+client.loop_start()
+# subscribe to incoming order
+client.subscribe("orders")
+client.subscribe("new-order")
+# keep loop running for 60 seconds
+time.sleep(6000)
+
+# disconnect and stop loop
+
+client.disconnect()
+client.loop_stop()
---
+++ Edgezone-QSR Demo/Flask App/app.py
@@ -0,0 +1,67 @@
+import json
+from flask import Flask, request, jsonify, render_template, make_response
+from flask_mqtt import Mqtt
+
+app = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = '13.105.113.2'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'client1'  # Set this item when you need to verify username and password
+app.config['MQTT_PASSWORD'] = 'password'  # Set this item when you need to verify username and password
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
+result = 'result'
+orders = 'orders'
+
+mqtt_client = Mqtt(app)
+
+results = []
+latency = []
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template('index.html')
+
+@app.route('/post_items_placement', methods=["POST"])
+def item_placement():
+    # process JSON
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        json_data = request.json
+        # sample json data
+        # {'bag_no': 1, 'item_name': 'soda', 'action': 'add'}
+        mqtt_client.publish(topic=orders, payload=json.dumps(json_data), qos=1, retain=False)
+        print (json.dumps(json_data))
+        return json_data
+    return 'Content-Type not supported'
+
+@app.route('/data', methods=["GET", "POST"])
+def data():
+    response = make_response(json.dumps(results))
+    response.content_type = 'application/json'
+    print(response)
+    results.clear()
+    return response
+
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected successfully')
+        mqtt_client.subscribe(result) # subscribe topic
+    else:
+        print('Bad connection. Code:', rc)
+
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+  )
+    print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+    if message.topic == "result":
+        results.append(data['payload'])
+    if message.topic == "latency":
+        latency.append(data['payload'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
