eb2d60118643969da59cf077b66e8085c01ff28c
Susan Seiler <47536604+saseiler@users.noreply.github.com>
2022-12-20 10:20:26 -0600

    cleaned up BL and made readme
    cleaned up the BL and created readme file

--- Edgezone-QSR Demo/Business Logic App/README.md
+++ Edgezone-QSR Demo/Business Logic App/README.md
@@ -1,2 +1,101 @@
 # Business Logic Application
 
+Business Logic (BL) application is a MQTT client app written in Python that connects to a MQTT broker deployed on the Edgezone. The BL app accepts new orders from the Point of Service (PoS) application and inference results from the Edge AI container both via MQTT topics. It then compares the two inputs to determine if the correct item is being added.  
+
+There are six business scenarios we have accounted for in this client:
+1. A new Customers' order is created in POS system and is in need of being fulfilled
+2. Item being added in the bag matches the POS order
+3. Item being added in the bag does not match the POS order
+4. Item being removed from the bag matches the POS order
+5. Item being removed from the bag does not match the POS order
+6. The order has been fulfilled and ready to give to customer
+
+
+A sample order would look as follows-
+```json
+{
+    "bag_no" : 1, 
+    "items" : [{
+        "French Fries" : 1, 
+        "Cheeseburger" : 1, 
+        "Soda" : 1, 
+        "Chicken Nuggets" : 1
+        }]
+}
+```
+A sample inference result/action-
+```json
+{
+    "bag_no": 1, 
+    "item_name": "soda", 
+    "action": "add"
+}
+```
+The BL application will process the incoming inference and the corresponding order. It will determine a Status True or False for the current action (**True** meaning the action is correct and **False** meaning the action is incorrect). The BL will publish the processed JSON to the flask server via MQTT topic.
+A sample post-processed JSON would look as follows-
+```json
+{
+    "bag_no": 1, 
+    "item_name": "soda", 
+    "action": "add",
+    "status": True
+}
+```
+
+The busines logic client will process the six use cases and return the following reponses:
+Use Case | BL Result
+--- | ---
+1. A new Customers' order is created in POS system and is in need of being fulfilled | No result returned
+2. Item being added in the bag matches the POS order | TRUE
+3. Item being added in the bag does not match the POS order | FALSE
+4. Item being removed from the bag matches the POS order | FALSE
+5. Item being removed from the bag does not match the POS order | TRUE
+6. The order has been fulfilled and ready to give to customer | "order-complete"
+
+
+## Preparing your image
+1. Get the code for the BL application from this repository
+
+`git clone https://github.com/microsoft/Azure-Edge-Solutions-Lab.git`
+
+2. Navigate to "Edgezone-QSR Demo"/"Business Logic App" directory. This directory will have the Dockerfile required to create the container image. Build your image using docker and give it a tag.
+
+`docker build tag <acrname>.azurecr.io/bl-client:v1 .`
+
+3. Log in to your Azure Container Registry
+
+`docker login -u <ACR username> -p <ACR password> <ACR login server>`
+
+4. Push the created image to Azure Container Registry
+
+`docker push <acrname>.azurecr.io/bl-client:v1`
+
+## Creating a secret on K8s cluster
+
+Run the following kubectl command on your K8s cluster
+
+`kubectl create secret docker-registry <secret-name> --namespace <namespace> --docker-server=<container-registry-name>.azurecr.io --docker-username=<service-principal-ID>     --docker-password=<service-principal-password>`
+
+Once the secret is created you can use it in the yaml file-
+
+```
+imagePullSecrets:
+    - name: acr-secret-edgezone
+```
+Look at the BL-client.yaml file in this directory for more information.
+
+## Deploy the BL-client application to K8s Cluster
+
+### Option 1: From PowerShell/Command Line
+Using the kubectl command on the K8s cluster running on the edgezone
+
+`kubectl apply -f BL-client.yaml`
+
+### Option 2: From Azure Arc
+TO-DO
+
+
+
+
+
+
--- Edgezone-QSR Demo/Business Logic App/business-logic-app.py
+++ Edgezone-QSR Demo/Business Logic App/business-logic-app.py
@@ -5,71 +5,80 @@ from datetime import datetime
 
 import paho.mqtt.client as mqtt
 
-broker_url = "13.105.113.2" # IP address of broker running on edgezone
+broker_url = "<mqtt broker IP address>" # IP address of broker running on edgezone
 broker_port = 1883
-orders = []
+pos_orders = []
 
-#define callback
+#method is called once a inbounded message is recieved on topic "orders"
 def on_message(client, userdata, message):
     
     #decode the incoming message 
-    # sample json data
-        # {'bag_no': 1, 'item_name': 'soda', 'action': 'add'}
+    # sample json data: {'bag_no': 1, 'item_name': 'soda', 'action': 'add'}
     json_in_order = json.loads(str(message.payload.decode("utf-8","ignore"))) #decode json data
-    print("Action: ", json_in_order)
-    # fetch order to check 
-    # {"bag_no" : 1, "items" : [{"French Fries" : 1, "Cheeseburger" : 1, "Soda" : 1, "Chicken Nuggets" : 1}]}
-    order = [""]
-    for i in range(0, len(orders)):
-        if orders[i]["bag_no"] == json_in_order["bag_no"]:
-            order = orders[i]["items"]
-    # order is a key-value hashmap
-    if json_in_order["item_name"] in order[0].keys():
+    print("Message Recieved: ", json_in_order)
+    
+    # initate a tempory list to compare the incoming item to POS order
+    # loads the POS order for bag number to compare
+    order_compare = [""]
+    for i in range(0, len(pos_orders)):
+        if pos_orders[i]["bag_no"] == json_in_order["bag_no"]:
+            order_compare = pos_orders[i]["items"]
+
+    # order is a key-value hashmap which is an associative array
+    # if item addded to bag is part of the order, make status true
+    # if item added to bag is not part of the order, status is false
+    if json_in_order["item_name"] in order_compare[0].keys():
         if json_in_order["action"] == "add":
             json_out = json_in_order
             json_out["Status"] = True
-            order[0][json_in_order["item_name"]] -= 1
-            if isOrderComplete(order[0]):
+            order_compare[0][json_in_order["item_name"]] -= 1
+            if isOrderComplete(order_compare[0]):
                 order_complete = {"bag_no": json_in_order["bag_no"], "Status": "order-completed"}
                 print("Messages to Publish: ", order_complete)
                 client.publish(topic="result", payload=json.dumps(order_complete), qos=1, retain=False)           
         elif json_in_order["action"] == "remove":
             json_out = json_in_order
             json_out["Status"] = False
-    elif json_in_order["item_name"] not in order[0].keys():
+    # if item added to bag is not in the order check if being added or removed from bag
+    # if being added and it's not part of order, status is false
+    # if being removed and is not part of the order, status is false
+    elif json_in_order["item_name"] not in order_compare[0].keys():
         if json_in_order["action"] == "add":
             json_out = json_in_order
             json_out["Status"] = False
         elif json_in_order["action"] == "remove":
             json_out = json_in_order
             json_out["Status"] = True
-            if isOrderComplete(order[0]):
+            if isOrderComplete(order_compare[0]):
                 order_complete = {"bag_no": json_in_order["bag_no"], "Status": "order-completed"}
                 print("Messages to Publish: ", order_complete)
                 client.publish(topic="result", payload=json.dumps(order_complete), qos=1, retain=False)
-            
-            
+                    
     print("Messages to Publish: ", json_out)    
     #publish the result to broker
     client.publish(topic="result", payload=json.dumps(json_out), qos=1, retain=False)
     
-
-def on_message_neworder(client, userdata, message):
+# method is called once a inbounded message is recieved on topic "new-order"
+def on_message_neworder(client, userdata, message):   
     #decode the incoming message 
     order = json.loads(str(message.payload.decode("utf-8","ignore"))) #decode json data
-    # {"bag_no" : 1, "items" : [{"French Fries" : 1, "Cheeseburger" : 1, "Soda" : 1, "Chicken Nuggets" : 1}]}
+    # sample json data: {"bag_no" : 1, "items" : [{"French Fries" : 1, "Cheeseburger" : 1, "Soda" : 1, "Chicken Nuggets" : 1}]}
     print("Order received from PoS application", order)
-    orders.append(order)
+    pos_orders.append(order)
 
+# method evaluate if order is complete
 def isOrderComplete(order):
     for value in order.values():
        if value >= 1:
         return False
     return True
 
+
 #configure the client and connect 
 client = mqtt.Client()
-#client.on_message = on_message
+
+#client subscribes to two topics "orders" the incoming items being placed in the bag
+# the second topic is "new-order" the actual order from the POS system
 client.message_callback_add("orders", on_message)
 client.message_callback_add("new-order", on_message_neworder)
 client.username_pw_set(username="client2", password="password2")
@@ -77,13 +86,13 @@ client.connect(broker_url, broker_port, keepalive=800)
 
 # start loop to process recieved messages
 client.loop_start()
-# subscribe to incoming order
+# subscribe to incoming order items as it is being filled
 client.subscribe("orders")
+# subscribe to the POS order 
 client.subscribe("new-order")
-# keep loop running for 60 seconds
+# keep loop running for n seconds
 time.sleep(6000)
 
 # disconnect and stop loop
-
 client.disconnect()
 client.loop_stop()
